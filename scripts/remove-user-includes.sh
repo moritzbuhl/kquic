b0e79da92493902f33a849d184edddbd66f937eb
Moritz Buhl <m.buhl@tum.de>
2023-06-26 16:46:36 +0200

    Fix whitespace.

--- scripts/remove-user-includes.sh
+++ scripts/remove-user-includes.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
-for h in inttypes.h stdarg.h stddef.h sys/types.h ngtcp2/version.h assert.h errno.h stdio.h string.h stdlib.h
+for h in inttypes.h stdarg.h stddef.h sys/types.h ngtcp2/version.h assert.h \
+	errno.h stdio.h string.h stdlib.h
 do
 	find ngtcp2 -name '*.[ch]' -exec sed -i -e \
 		"s,^#[ 	]*include[ 	]*<$h>,,g" {} \;
