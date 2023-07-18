7a94fa5ecf15c3ce85bccc9ab84e6a4b9cdb7b89
Moritz Buhl <m.buhl@tum.de>
2023-07-18 15:46:38 +0200

    Update possibly included libraries.

--- scripts/remove-user-includes.sh
+++ scripts/remove-user-includes.sh
@@ -3,7 +3,7 @@
 set -eux
 
 for h in inttypes.h stdarg.h stddef.h sys/types.h ngtcp2/version.h assert.h \
-	errno.h stdio.h string.h stdlib.h
+	errno.h stdio.h string.h stdlib.h netinet/in.h
 do
 	find ngtcp2 -name '*.[ch]' -exec sed -i -e \
 		"s,^#[ 	]*include[ 	]*<$h>,,g" {} \;
