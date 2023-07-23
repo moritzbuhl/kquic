46f4df03890def44de5909b85b40d4d34fdaeb65
Moritz Buhl <m.buhl@tum.de>
2023-07-23 20:46:04 +0200

    Remove more includes.

--- scripts/remove-user-includes.sh
+++ scripts/remove-user-includes.sh
@@ -3,7 +3,7 @@
 set -eux
 
 for h in inttypes.h stdarg.h stddef.h sys/types.h ngtcp2/version.h assert.h \
-	errno.h stdio.h string.h stdlib.h netinet/in.h
+	errno.h stdio.h string.h stdlib.h netinet/in.h sys/socket.h
 do
 	find ngtcp2 -name '*.[ch]' -exec sed -i -e \
 		"s,^#[ 	]*include[ 	]*<$h>,,g" {} \;
