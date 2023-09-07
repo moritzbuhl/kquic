2445d94780c17d9a851b9486e2397a3ba3fd3364
Moritz Buhl <m.buhl@tum.de>
2023-09-07 10:28:17 +0200

    Add logging to quic_v4_get_port.

--- quic.c
+++ quic.c
@@ -318,6 +318,7 @@ int quic_v4_get_port(struct sock *sk, unsigned short snum)
 	unsigned int hash2_partial =
 		ipv4_portaddr_hash(sock_net(sk), inet_sk(sk)->inet_rcv_saddr, 0);
 
+	pr_info("%s\n", __func__);
 	udp_sk(sk)->udp_portaddr_hash = hash2_partial;
 	return udp_lib_get_port(sk, snum, hash2_nulladdr);
 }
